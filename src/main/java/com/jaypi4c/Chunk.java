17b5baae544235dab8490fa6e097f46e7f9935e0
Jonas Pohl <jonas.pohl@uni-oldenburg.de>
2023-08-31 14:19:05 +0200

    auto format

--- src/main/java/com/jaypi4c/Chunk.java
+++ src/main/java/com/jaypi4c/Chunk.java
@@ -3,7 +3,7 @@ package com.jaypi4c;
 import java.awt.geom.Line2D;
 
 record Chunk(int minX, int minY, int maxX, int maxY) {
-public boolean isWithin(Line2D.Float line){
-    return line.intersectsLine(minX, minY, maxX, maxY);
-}
+    public boolean isWithin(Line2D.Float line) {
+        return line.intersectsLine(minX, minY, maxX, maxY);
+    }
 }
