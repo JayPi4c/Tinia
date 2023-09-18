9846e57e0efd12c27919c5c85037410db3634669
Jonas Pohl <jonas.pohl@uni-oldenburg.de>
2023-09-18 15:33:35 +0200

    Adding LD to Darreichungsform-Erkennung

--- src/main/java/com/jaypi4c/openehr/compositions/nephromedikationcomposition/NephroMedikationCompositionFactory.java
+++ src/main/java/com/jaypi4c/openehr/compositions/nephromedikationcomposition/NephroMedikationCompositionFactory.java
@@ -6,10 +6,17 @@ import com.jaypi4c.openehr.compositions.nephromedikationcomposition.definition.*
 import com.nedap.archie.rm.generic.PartyIdentified;
 import com.nedap.archie.rm.generic.PartySelf;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.text.similarity.LevenshteinDistance;
 import org.ehrbase.client.classgenerator.shareddefinition.Language;
 import org.ehrbase.client.classgenerator.shareddefinition.Setting;
 import org.ehrbase.client.classgenerator.shareddefinition.Territory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +30,7 @@ import java.util.List;
  *
  * @see <a href="https://ckm.highmed.org/ckm/templates/1246.169.1019">Nephro_Medikation in CKM</a> for more information.
  */
+@Slf4j
 public class NephroMedikationCompositionFactory implements ICompositionFactory<NephroMedikationComposition> {
 
     public NephroMedikationComposition createComposition(String[][] medicationMatrix) {
@@ -32,6 +40,8 @@ public class NephroMedikationCompositionFactory implements ICompositionFactory<N
         FallidentifikationCluster fallidentifikation = new FallidentifikationCluster();
         fallidentifikation.setFallKennungValue("Medikationsplan 1");
         composition.setFallidentifikation(fallidentifikation);
+
+        String[] formDict = loadDictionary("/dictionaries/DarreichungsformAllowlist.txt");
 
 
         // jede Zeile in der Tabelle ist eine Verordnung. So kann eine ganze Tabelle in einer composition gespeichert werden.
@@ -43,6 +53,9 @@ public class NephroMedikationCompositionFactory implements ICompositionFactory<N
             String handelsname = row[1].trim();
             String staerke = row[2].trim();
             String form = row[3].trim();
+            LDResult result = findClosestWord(form, formDict);
+            form = result.closestWord();
+            log.info("Found {} for {}", form, result.targetWord());
 
 
             // TODO: extract information from matrix and put into composition
@@ -56,7 +69,7 @@ public class NephroMedikationCompositionFactory implements ICompositionFactory<N
             ArzneimittelCluster arzneimittelCluster = new ArzneimittelCluster();
 
             ArzneimittelDarreichungsformElement arzneimittelDarreichungsformElement = new ArzneimittelDarreichungsformElement();
-            arzneimittelDarreichungsformElement.setValue(form + "en");
+            arzneimittelDarreichungsformElement.setValue(form);
             arzneimittelCluster.setDarreichungsform(List.of(arzneimittelDarreichungsformElement));
 
             WirkstoffCluster wirkstoffCluster = new WirkstoffCluster();
@@ -74,6 +87,40 @@ public class NephroMedikationCompositionFactory implements ICompositionFactory<N
 
         return composition;
     }
+
+    private LDResult findClosestWord(String word, String[] dict) {
+        final LevenshteinDistance ld = LevenshteinDistance.getDefaultInstance();
+
+        int minDistance = Integer.MAX_VALUE;
+        String closestWord = "";
+
+        for (String dictWord : dict) {
+            int distance = ld.apply(word, dictWord);
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestWord = dictWord;
+                if (minDistance == 0) // we found exact match
+                    break;
+            }
+        }
+        return new LDResult(minDistance, closestWord, word);
+    }
+
+    private String[] loadDictionary(String path) {
+        List<String> lines = new ArrayList<>();
+
+        try (InputStream is = NephroMedikationCompositionFactory.class.getResourceAsStream(path);
+             BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            log.error("Failed to load dictionary", e);
+        }
+        return lines.toArray(new String[0]);
+    }
+
 
     private static VerordnungVonArzneimittelInstruction prepareInstruction(VerordnungVonArzneimittelInstruction instruction) {
         instruction.setLanguage(Language.DE);
@@ -105,6 +152,12 @@ public class NephroMedikationCompositionFactory implements ICompositionFactory<N
         composition.setHealthCareFacility(healthCareFacility);
 
         return composition;
+    }
+
+    public record LDResult(int distance, String closestWord, String targetWord) {
+        public boolean exactMatch() {
+            return distance == 0;
+        }
     }
 
 
