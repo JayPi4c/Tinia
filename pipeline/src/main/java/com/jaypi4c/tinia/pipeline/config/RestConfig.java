9dc3512b3eadde03974d1ae287857f01b07de4d5
Jonas Pohl <jonas.jp4c@gmail.com>
2024-06-19 17:27:49 +0200

    Use httpClient for auth

--- pipeline/src/main/java/com/jaypi4c/tinia/pipeline/config/RestConfig.java
+++ pipeline/src/main/java/com/jaypi4c/tinia/pipeline/config/RestConfig.java
@@ -1,7 +1,15 @@
 package com.jaypi4c.tinia.pipeline.config;
 
 import com.jaypi4c.tinia.pipeline.openehr.compositions.nephromedikationcomposition.NephroMedikationTemplateProvider;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.ehrbase.openehr.sdk.client.openehrclient.OpenEhrClient;
 import org.ehrbase.openehr.sdk.client.openehrclient.OpenEhrClientConfig;
+import org.ehrbase.openehr.sdk.client.openehrclient.defaultrestclient.DefaultRestClient;
 import org.ehrbase.openehr.sdk.webtemplate.templateprovider.TemplateProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +24,11 @@ public class RestConfig {
     @Value("${openehr.url:localhost}")
     private String OPEN_EHR_URL;
 
+    @Value("${openehr.username}")
+    private String username;
+    @Value("${openehr.password}")
+    private String password;
+
     @Bean
     public TemplateProvider getTemplateProvider() {
         return new NephroMedikationTemplateProvider();
@@ -28,6 +41,17 @@ public class RestConfig {
         } catch (URISyntaxException e) {
             return null;
         }
+    }
+
+    @Bean
+    public OpenEhrClient getDefaultRestClient(OpenEhrClientConfig config, TemplateProvider templateProvider){
+        CredentialsProvider provider = new BasicCredentialsProvider();
+        provider.setCredentials(
+                AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password)
+        );
+        HttpClient hc = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
+        return new DefaultRestClient(config, templateProvider, hc);
     }
 
 }
