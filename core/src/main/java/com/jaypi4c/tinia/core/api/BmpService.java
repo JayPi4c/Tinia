d6c71ed987519ec802531b645a13c223373d14d4
Jonas Pohl <jonas.jp4c@gmail.com>
2025-03-05 18:40:44 +0100

    Executing pipeline on api call

--- core/src/main/java/com/jaypi4c/tinia/core/api/BmpService.java
+++ core/src/main/java/com/jaypi4c/tinia/core/api/BmpService.java
@@ -1,20 +1,33 @@
 package com.jaypi4c.tinia.core.api;
 
+import com.jaypi4c.tinia.core.Pipeline;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class BmpService implements BmpApiDelegate {
+
+    private final Pipeline pipeline;
 
     @Override
     public ResponseEntity<Void> extractBmp(List<MultipartFile> files) {
-        System.out.println(files.size() + " files received");
-        files.stream().map(MultipartFile::getOriginalFilename).forEach(System.out::println);
-        return BmpApiDelegate.super.extractBmp(files);
+        try {
+            for (MultipartFile file : files) {
+                String name = file.getOriginalFilename();
+                pipeline.process(file.getInputStream(), name);
+            }
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
