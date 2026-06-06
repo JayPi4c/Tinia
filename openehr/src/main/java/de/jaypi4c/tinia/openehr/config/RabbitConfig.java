32e75d39be5c2e9c17429d25872efedcec2b9a19
Jonas Pohl <git@jaypi4c.de>
2026-06-06 14:38:55 +0200

    Adding openehr service to compose

--- openehr/src/main/java/de/jaypi4c/tinia/openehr/config/RabbitConfig.java
+++ openehr/src/main/java/de/jaypi4c/tinia/openehr/config/RabbitConfig.java
@@ -1,7 +1,7 @@
 package de.jaypi4c.tinia.openehr.config;
 
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,7 +22,7 @@ public class RabbitConfig {
     }
 
     @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public JacksonJsonMessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
     }
 }
