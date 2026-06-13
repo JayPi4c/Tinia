ed0b6027a99071b6535816fde171b08546bc9914
Jonas Pohl <git@jaypi4c.de>
2026-06-13 21:52:57 +0200

    Adding new module detector

---
+++ detector/src/main/java/de/jaypi4c/tinia/detector/DetectorApp.java
@@ -0,0 +1,13 @@
+package de.jaypi4c.tinia.detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectorApp {
+
+    static void main(String[] args) {
+        SpringApplication.run(DetectorApp.class, args);
+    }
+
+}
